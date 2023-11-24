b101fef74023dcc00b57b022f5597097013df703
yoon-gang00 <yk1008@konkuk.ac.kr>
2023-11-24 11:08:59 +0900

    스플래쉬 화면

--- app/src/main/java/com/example/hand/LogoActivity.kt
+++ app/src/main/java/com/example/hand/LogoActivity.kt
@@ -1,4 +1,21 @@
 package com.example.hand
 
-class LogoActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class LogoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo)
+
+        // 2초 후 MainActivity로 이동
+        Handler().postDelayed({
+            val intent = Intent(this, NaviActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
 }
