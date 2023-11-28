b36a26ee10a7eea7ac9352877febcfc08987567c
yoon-gang00 <yk1008@konkuk.ac.kr>
2023-11-28 13:34:06 +0900

    날씨 아이콘 추가, 날씨 화면

--- app/src/main/java/com/example/hand/MyFragment.kt
+++ app/src/main/java/com/example/hand/MyFragment.kt
@@ -1,11 +1,14 @@
 package com.example.hand
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.example.hand.databinding.FragmentMyBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,26 +20,36 @@ import android.widget.EditText
  * create an instance of this fragment.
  */
 class MyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
 
-    private lateinit var nameEditText: EditText
-    private lateinit var dateOfBirthEditText: EditText
-    private lateinit var timeOfBirthEditText: EditText
-    private lateinit var genderEditText: EditText
+    private lateinit var binding: FragmentMyBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ) : View? {
-        val view = inflater.inflate(R.layout.fragment_my, container, false)
-
-        nameEditText = view.findViewById(R.id.nameEditText)
-        dateOfBirthEditText = view.findViewById(R.id.dateOfBirthEditText)
-        timeOfBirthEditText = view.findViewById(R.id.timeOfBirthEditText)
-        genderEditText = view.findViewById(R.id.genderEditText)
-
-        return view
+        binding = FragmentMyBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.submitButton.setOnClickListener {
+            showConfirmationDialog()
+        }
+    }
+
+    private fun showConfirmationDialog() {
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("저장하시겠습니까?")
+            .setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+                // 여기에 저장 로직을 추가하세요.
+            })
+            .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, which ->
+                // 여기에 취소 로직을 추가하세요.
+            })
+            .show()
+    }
 
 
     companion object {
